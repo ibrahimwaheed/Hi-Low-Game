f3473e1a63cfd3ff5f92148549d65986e4c79237
ibrahimwaheed <43203098+ibrahimwaheed@users.noreply.github.com>
2018-11-27 13:33:42 +0000

    Add files via upload

---
+++ HiLowGame.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int cNumber;
+	cNumber = rand() % 10 + 1;
+  cout << "The Computer's number is :"<< cNumber << endl;
+	int guess;
+	guess = rad () % 10 + 1;
+	cout << "Will your number be Higher or Lower?: ";
+	return 0;
+}
